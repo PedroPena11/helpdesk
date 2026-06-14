ed326524c702015fd8bd7f2fcd22bfaa4fe24e27
Pedro Peña <pedroal247@gmail.com>
2026-06-14 11:56:40 -0400

    Modificada la base de datos para permitir el uso de roles

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,26 +3,42 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
-
     /**
      * Seed the application's database.
      */
     public function run(): void
     {
-        if(!User::where('email','admin@helpdesk.com')->exists()){
+        
+        if (!User::where('email', 'admin@helpdesk.com')->exists()) {
             User::create([
-                'name' => 'Administrator Support',
+                'name' => 'Admin',
                 'email' => 'admin@helpdesk.com',
                 'password' => Hash::make('SeguridadLaravel2026*'),
+                'role' => 'admin',
             ]);
         }
 
+        if (!User::where('email', 'tecnico@helpdesk.com')->exists()) {
+            User::create([
+                'name' => 'Carlos Mendoza (Técnico Redes)',
+                'email' => 'tecnico@helpdesk.com',
+                'password' => Hash::make('Tecnico2026*'),
+                'role' => 'agent',
+            ]);
+        }
+
+        if (!User::where('email', 'cliente@example.com')->exists()) {
+            User::create([
+                'name' => 'Juan Perez (Cliente)',
+                'email' => 'cliente@example.com',
+                'password' => Hash::make('Cliente2026*'),
+                'role' => 'client',
+            ]);
+        }
     }
 }
